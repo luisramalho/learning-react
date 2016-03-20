ddc07a5c7513f73e89806ba43beef804259fd0ce
Luís Ramalho <info@luisramalho.com>
2016-03-20 11:03:56 +0200

    Simple TODO app

--- app/App.js
+++ app/App.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 import {render} from 'react-dom';
 
-class App extends Component {
-  render(){
+var TodoList = React.createClass({
+  render: function() {
+    var createItem = function(item) {
+      return <li key={item.id}>{item.text}</li>;
+    };
+    return <ul>{this.props.items.map(createItem)}</ul>;
+  }
+});
+
+var TodoApp = React.createClass({
+  getInitialState: function() {
+    return {items: [], text: ''};
+  },
+  onChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var nextItems = this.state.items.concat([{text: this.state.text, id: Date.now()}]);
+    var nextText = '';
+    this.setState({items: nextItems, text: nextText});
+  },
+  render: function() {
     return (
-      <h1>Hello World</h1>
+      <div>
+        <h3>TODO</h3>
+        <TodoList items={this.state.items} />
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.onChange} value={this.state.text} />
+          <button>{'Add #' + (this.state.items.length + 1)}</button>
+        </form>
+      </div>
     );
   }
-}
+});
 
-render(<App />, document.getElementById('root'));
+render(<TodoApp />, document.getElementById('root'));
