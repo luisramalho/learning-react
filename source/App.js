8e678710299ff937f3181b4782ce129ff965e798
Luís Ramalho <info@luisramalho.com>
2016-04-08 15:22:16 +0300

    Started building the Kanban board

--- source/App.js
+++ source/App.js
@@ -1,26 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {render} from 'react-dom';
+import KanbanBoard from './KanbanBoard';
 
-class GroceryList extends Component {
-  render() {
-    return (
-      <ul>
-        <ListItem quantity="1" name="Bread" />
-        <ListItem quantity="6" name="Eggs" />
-        <ListItem quantity="2" name="Milk" />
-      </ul>
-    )
+let cardsList = [
+  {
+    id: 1,
+    title: "Read the Book",
+    description: "I should read the whole book",
+    status: "in-progress",
+    tasks: []
+  },
+  {
+    id: 2,
+    title: "Write some code",
+    description: "Code along with the samples in the book",
+    status: "todo",
+    tasks: [
+      {
+        id: 1,
+        name: "ContactList Example",
+        done: true
+      },
+      {
+        id: 2,
+        name: "Kanban Example",
+        done: false
+      },
+      {
+        id: 3,
+        name: "My own experiments",
+        done: false
+      }
+    ]
   }
-}
+];
 
-class ListItem extends Component {
-  render() {
-    return (
-      <li>
-        {this.props.quantity}x {this.props.name}
-      </li>
-    )
-  }
-}
-
-render(<GroceryList />, document.getElementById('root'));
+render(<KanbanBoard cards={cardsList} />, document.getElementById('root'));
